359af641a361e060fc44f58645455ab0e2b64305
chihunmanse <chihunmanse@gmail.com>
2021-11-02 19:23:04 +0900

    [ADD]: postings/comments view

--- postings/urls.py
+++ postings/urls.py
@@ -2,9 +2,14 @@ from django.urls    import path
 
 from postings.views import (
     PostingView,
-    PostingParamView
+    PostingParamView,
+    CommentView,
+    CommentDetailView,
 )
+
 urlpatterns =[
     path('', PostingView.as_view()), 
-    path('/<int:posting_id>', PostingParamView.as_view())  
+    path('/<int:posting_id>', PostingParamView.as_view()),
+    path('/comments/<int:posting_id>', CommentView.as_view()),
+    path('/comment/<int:comment_id>', CommentDetailView.as_view()),  
 ]
